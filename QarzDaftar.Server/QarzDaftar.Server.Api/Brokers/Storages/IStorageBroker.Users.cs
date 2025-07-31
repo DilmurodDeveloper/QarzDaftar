73549774e2a37bfa8c03e59c3b29afcc0b550107
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-31 23:23:44 +0500

    BROKERS: Insert User

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/IStorageBroker.Users.cs
@@ -0,0 +1,9 @@
+﻿using QarzDaftar.Server.Api.Models.Foundations.Users;
+
+namespace QarzDaftar.Server.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<User> InsertUserAsync(User user);
+    }
+}
