d91f7639f6caa660dc80b9edc828e15932803c10
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-02 00:30:53 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursLogItAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Exceptions.Add.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.Users;
+using QarzDaftar.Server.Api.Models.Foundations.Users.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursLogItAsync()
+        {
+            // given
+            User someUser = CreateRandomUser();
+            SqlException sqlException = GetSqlError();
+
+            var failedUserStorageException =
+                new FailedUserStorageException(sqlException);
+
+            var expectedUserDependencyException =
+                new UserDependencyException(failedUserStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertUserAsync(someUser))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<User> addUserTask =
+                this.userService.AddUserAsync(someUser);
+
+            // then
+            await Assert.ThrowsAsync<UserDependencyException>(() =>
+                addUserTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(someUser), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedUserDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
