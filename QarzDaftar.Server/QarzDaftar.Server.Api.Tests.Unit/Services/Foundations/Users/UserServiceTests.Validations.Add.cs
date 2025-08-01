00ac32495cc954457a9175124e04fb05c258104a
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-01 23:20:09 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.Users;
+using QarzDaftar.Server.Api.Models.Foundations.Users.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync()
+        {
+            // given
+            User nullUser = null;
+            var nullUserException = new NullUserException();
+
+            var expectedUserValidationException =
+                new UserValidationException(nullUserException);
+
+            // when
+            ValueTask<User> addUserTask =
+                this.userService.AddUserAsync(nullUser);
+
+            UserValidationException actualUserException =
+                await Assert.ThrowsAsync<UserValidationException>(addUserTask.AsTask);
+
+            // then
+            actualUserException.Should().BeEquivalentTo(expectedUserValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(
+                expectedUserValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(It.IsAny<User>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Users/UserServiceTests.cs
@@ -1,10 +1,12 @@
-﻿using Moq;
+﻿using System.Linq.Expressions;
+using Moq;
 using QarzDaftar.Server.Api.Brokers.DateTimes;
 using QarzDaftar.Server.Api.Brokers.Loggings;
 using QarzDaftar.Server.Api.Brokers.Storages;
 using QarzDaftar.Server.Api.Models.Foundations.Users;
 using QarzDaftar.Server.Api.Services.Foundatios.Users;
 using Tynamix.ObjectFiller;
+using Xeptions;
 
 namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Users
 {
@@ -31,6 +33,9 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Users
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
 
         private static Filler<User> CreateUserFiller(DateTimeOffset date)
         {
