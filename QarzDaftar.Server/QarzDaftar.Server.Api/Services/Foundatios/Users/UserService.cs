b27993cbc4410bb5e261a3d68c46782d0b8077ef
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-02 00:40:31 +0500

    ShouldThrowDependencyValidationExceptionOnAddIfDuplicateKeyErrorOccursAndLogItAsync -> PASS

--- QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundatios/Users/UserService.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundatios/Users/UserService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using EFxceptions.Models.Exceptions;
+using Microsoft.Data.SqlClient;
 using QarzDaftar.Server.Api.Brokers.DateTimes;
 using QarzDaftar.Server.Api.Brokers.Loggings;
 using QarzDaftar.Server.Api.Brokers.Storages;
@@ -60,6 +61,18 @@ namespace QarzDaftar.Server.Api.Services.Foundatios.Users
                 this.loggingBroker.LogCritical(userDependencyException);
 
                 throw userDependencyException;
+            }
+            catch (DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistsUserException =
+                    new AlreadyExistsUserException(duplicateKeyException);
+
+                var userDependencyValidationException =
+                    new UserDependencyValidationException(alreadyExistsUserException);
+
+                this.loggingBroker.LogError(userDependencyValidationException);
+
+                throw userDependencyValidationException;
             }
         }
     }
