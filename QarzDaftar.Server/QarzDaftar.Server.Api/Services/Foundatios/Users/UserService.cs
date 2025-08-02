4fe1cd0cb115c011da6c87e30ac353fb3cbca5e3
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-02 13:12:56 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> PASS

--- QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundatios/Users/UserService.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundatios/Users/UserService.cs
@@ -1,7 +1,9 @@
-﻿using QarzDaftar.Server.Api.Brokers.DateTimes;
+﻿using Microsoft.Data.SqlClient;
+using QarzDaftar.Server.Api.Brokers.DateTimes;
 using QarzDaftar.Server.Api.Brokers.Loggings;
 using QarzDaftar.Server.Api.Brokers.Storages;
 using QarzDaftar.Server.Api.Models.Foundations.Users;
+using QarzDaftar.Server.Api.Models.Foundations.Users.Exceptions;
 
 namespace QarzDaftar.Server.Api.Services.Foundatios.Users
 {
@@ -29,7 +31,24 @@ namespace QarzDaftar.Server.Api.Services.Foundatios.Users
             return await this.storageBroker.InsertUserAsync(user);
         });
 
-        public IQueryable<User> RetrieveAllUsers() =>
-            this.storageBroker.SelectAllUsers();
+        public IQueryable<User> RetrieveAllUsers()
+        {
+            try
+            {
+                return this.storageBroker.SelectAllUsers();
+            }
+            catch (SqlException sqlException)
+            {
+                var failedUserStorageException =
+                    new FailedUserStorageException(sqlException);
+
+                var userDependencyException =
+                    new UserDependencyException(failedUserStorageException);
+
+                this.loggingBroker.LogCritical(userDependencyException);
+
+                throw userDependencyException;
+            }
+        }
     }
 }
