e56e7ea33cb21218b2ee2fd96eb4c16578f3ba1d
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-02 19:32:44 +0500

    ShouldThrowDependencyValidationOnRemoveIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> PASS

--- QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundatios/Users/UserService.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundatios/Users/UserService.cs
@@ -1,4 +1,5 @@
-﻿using QarzDaftar.Server.Api.Brokers.DateTimes;
+﻿using Microsoft.EntityFrameworkCore;
+using QarzDaftar.Server.Api.Brokers.DateTimes;
 using QarzDaftar.Server.Api.Brokers.Loggings;
 using QarzDaftar.Server.Api.Brokers.Storages;
 using QarzDaftar.Server.Api.Models.Foundations.Users;
@@ -89,6 +90,18 @@ namespace QarzDaftar.Server.Api.Services.Foundatios.Users
                 this.loggingBroker.LogError(userValidationException);
 
                 throw userValidationException;
+            }
+            catch (DbUpdateConcurrencyException dbUpdateConcurrencyException)
+            {
+                var lockedUserException =
+                    new LockedUserException(dbUpdateConcurrencyException);
+
+                var userDependencyValidationException =
+                    new UserDependencyValidationException(lockedUserException);
+
+                this.loggingBroker.LogError(userDependencyValidationException);
+
+                throw userDependencyValidationException;
             }
         }
     }
