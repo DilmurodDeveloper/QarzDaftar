6e86af4b5fcb451927eb03a6fe0d7a30cc1b7d01
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-02 11:57:02 +0500

    EXPOSERS: Post User

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QarzDaftar.Server.Api.Models.Foundations.Users;
+using QarzDaftar.Server.Api.Models.Foundations.Users.Exceptions;
+using QarzDaftar.Server.Api.Services.Foundatios.Users;
+using RESTFulSense.Controllers;
+
+namespace QarzDaftar.Server.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : RESTFulController
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService) =>
+            this.userService = userService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<User>> PostUserAsync(User user)
+        {
+            try
+            {
+                User addUser = await this.userService.AddUserAsync(user);
+
+                return Created(addUser);
+            }
+            catch (UserValidationException userValidationException)
+            {
+                return BadRequest(userValidationException.InnerException);
+            }
+            catch (UserDependencyValidationException userDependencyValidationException)
+                when (userDependencyValidationException.InnerException is AlreadyExistsUserException)
+            {
+                return Conflict(userDependencyValidationException.InnerException);
+            }
+            catch (UserDependencyException userDependencyException)
+            {
+                return InternalServerError(userDependencyException.InnerException);
+            }
+            catch (UserServiceException userServiceException)
+            {
+                return InternalServerError(userServiceException.InnerException);
+            }
+        }
+    }
+}
