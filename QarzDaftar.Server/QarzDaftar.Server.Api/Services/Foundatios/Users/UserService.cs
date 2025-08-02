38b2b6323b2a1c77a25d101c3ba308e757c3d01d
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-02 19:37:23 +0500

    ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync -> PASS

--- QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundatios/Users/UserService.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundatios/Users/UserService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using QarzDaftar.Server.Api.Brokers.DateTimes;
 using QarzDaftar.Server.Api.Brokers.Loggings;
 using QarzDaftar.Server.Api.Brokers.Storages;
@@ -102,6 +103,18 @@ namespace QarzDaftar.Server.Api.Services.Foundatios.Users
                 this.loggingBroker.LogError(userDependencyValidationException);
 
                 throw userDependencyValidationException;
+            }
+            catch (SqlException sqlException)
+            {
+                var failedUserStorageException =
+                    new FailedUserStorageException(sqlException);
+
+                var userDependencyException =
+                    new UserDependencyException(failedUserStorageException);
+
+                this.loggingBroker.LogCritical(userDependencyException);
+
+                throw userDependencyException;
             }
         }
     }
