f41119bbc22d90de8f67de1296806adba6dbff53
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 00:04:40 +0500

    ShouldThrowNotFoundExceptionOnRemoveDebtByIdIsNotFoundAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Debts/DebtServiceTests.Validations.RemoveById.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Debts/DebtServiceTests.Validations.RemoveById.cs
@@ -46,5 +46,50 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Debts
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemoveDebtByIdIsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid inputDebtId = Guid.NewGuid();
+            Debt noDebt = null;
+            var notFoundDebtException = new NotFoundDebtException(inputDebtId);
+
+            var expectedDebtValidationException =
+                new DebtValidationException(notFoundDebtException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectDebtByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noDebt);
+
+            // when
+            ValueTask<Debt> removeDebtById =
+                this.debtService.RemoveDebtByIdAsync(inputDebtId);
+
+            var actualDebtValidationException =
+                await Assert.ThrowsAsync<DebtValidationException>(
+                    removeDebtById.AsTask);
+
+            // then
+            actualDebtValidationException.Should()
+                .BeEquivalentTo(expectedDebtValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectDebtByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDebtValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteDebtAsync(It.IsAny<Debt>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
