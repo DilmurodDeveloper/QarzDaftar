c84fc49c26f39caf93242f69955c6e8aace95192
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-03 13:23:53 +0500

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Customers/CustomerServiceTests.Exceptions.Modify.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Customers/CustomerServiceTests.Exceptions.Modify.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using QarzDaftar.Server.Api.Models.Foundations.Customers;
 using QarzDaftar.Server.Api.Models.Foundations.Customers.Exceptions;
@@ -40,6 +41,51 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Customers
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedCustomerDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCustomerByIdAsync(customerId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateCustomerAsync(someCustomer), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            Customer randomCustomer = CreateRandomCustomer();
+            Customer someCustomer = randomCustomer;
+            Guid customerId = someCustomer.Id;
+            var databaseUpdateException = new DbUpdateException();
+
+            var failedCustomerStorageException =
+                new FailedCustomerStorageException(databaseUpdateException);
+
+            var expectedCustomerDependencyException =
+                new CustomerDependencyException(failedCustomerStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCustomerByIdAsync(customerId)).Throws(databaseUpdateException);
+
+            // when
+            ValueTask<Customer> modifyCustomerTask =
+                this.customerService.ModifyCustomerAsync(someCustomer);
+
+            CustomerDependencyException actualCustomerDependencyException =
+                 await Assert.ThrowsAsync<CustomerDependencyException>(
+                    modifyCustomerTask.AsTask);
+
+            // then
+            actualCustomerDependencyException.Should()
+                .BeEquivalentTo(expectedCustomerDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
                     expectedCustomerDependencyException))), Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
