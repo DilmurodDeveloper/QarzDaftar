9be488901b32791677f4542b091fb6a43354915f
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-03 15:28:21 +0500

    BROKERS: Insert Debt

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/IStorageBroker.Debts.cs
@@ -0,0 +1,9 @@
+﻿using QarzDaftar.Server.Api.Models.Foundations.Debts;
+
+namespace QarzDaftar.Server.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Debt> InsertDebtAsync(Debt debt);
+    }
+}
