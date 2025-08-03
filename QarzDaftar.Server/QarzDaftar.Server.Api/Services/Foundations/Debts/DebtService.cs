f8988568df2889029ec413d2d79230ed2bc8e533
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-03 21:15:40 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> PASS

--- QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundations/Debts/DebtService.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundations/Debts/DebtService.cs
@@ -1,7 +1,9 @@
-﻿using QarzDaftar.Server.Api.Brokers.DateTimes;
+﻿using Microsoft.Data.SqlClient;
+using QarzDaftar.Server.Api.Brokers.DateTimes;
 using QarzDaftar.Server.Api.Brokers.Loggings;
 using QarzDaftar.Server.Api.Brokers.Storages;
 using QarzDaftar.Server.Api.Models.Foundations.Debts;
+using QarzDaftar.Server.Api.Models.Foundations.Debts.Exceptions;
 
 namespace QarzDaftar.Server.Api.Services.Foundations.Debts
 {
@@ -29,7 +31,24 @@ namespace QarzDaftar.Server.Api.Services.Foundations.Debts
             return await this.storageBroker.InsertDebtAsync(debt);
         });
 
-        public IQueryable<Debt> RetrieveAllDebts() =>
-            this.storageBroker.SelectAllDebts();
+        public IQueryable<Debt> RetrieveAllDebts()
+        {
+            try
+            {
+                return this.storageBroker.SelectAllDebts();
+            }
+            catch (SqlException sqlException)
+            {
+                var failedDebtStorageException =
+                    new FailedDebtStorageException(sqlException);
+
+                var debtDependencyException =
+                    new DebtDependencyException(failedDebtStorageException);
+
+                this.loggingBroker.LogCritical(debtDependencyException);
+
+                throw debtDependencyException;
+            }
+        }
     }
 }
