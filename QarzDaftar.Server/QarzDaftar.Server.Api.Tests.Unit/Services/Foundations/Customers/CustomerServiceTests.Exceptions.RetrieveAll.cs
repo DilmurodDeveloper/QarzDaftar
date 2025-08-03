0b4028a952d7a8da13619b2702ec3c99bd4c7f7e
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-03 10:27:28 +0500

    ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Customers/CustomerServiceTests.Exceptions.RetrieveAll.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Customers/CustomerServiceTests.Exceptions.RetrieveAll.cs
@@ -45,5 +45,44 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Customers
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string exceptionMessage = GetRandomString();
+            var serverException = new Exception(exceptionMessage);
+
+            var failedCustomerServiceException =
+                new FailedCustomerServiceException(serverException);
+
+            var expectedCustomerServiceException =
+                new CustomerServiceException(failedCustomerServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllCustomers()).Throws(serverException);
+
+            // when 
+            Action retrieveAllCustomerActions = () =>
+                this.customerService.RetrieveAllCustomers();
+
+            CustomerServiceException actualCustomerServiceException =
+                Assert.Throws<CustomerServiceException>(retrieveAllCustomerActions);
+
+            // then
+            actualCustomerServiceException.Should()
+                .BeEquivalentTo(expectedCustomerServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllCustomers(), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedCustomerServiceException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
