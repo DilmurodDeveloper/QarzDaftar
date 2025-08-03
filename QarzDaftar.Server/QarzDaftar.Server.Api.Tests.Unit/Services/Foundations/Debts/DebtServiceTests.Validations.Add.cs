f503076be8f73630c03b24f90a3eaea99d2fe013
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-03 16:25:27 +0500

    ShouldThrowValidationExceptionOnAddIfDebtIsInvalidAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Debts/DebtServiceTests.Validations.Add.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Debts/DebtServiceTests.Validations.Add.cs
@@ -39,5 +39,80 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Debts
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfDebtIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidDebt = new Debt
+            {
+                Description = invalidText,
+            };
+
+            var invalidDebtException = new InvalidDebtException();
+
+            invalidDebtException.AddData(
+                key: nameof(Debt.Id),
+                values: "Id is required");
+
+            invalidDebtException.AddData(
+                key: nameof(Debt.Amount),
+                values: "Amount is required");
+
+            invalidDebtException.AddData(
+                key: nameof(Debt.Description),
+                values: "Text is required");
+
+            invalidDebtException.AddData(
+                key: nameof(Debt.DueDate),
+                values: "Date is required");
+
+            invalidDebtException.AddData(
+                key: nameof(Debt.CreatedDate),
+                values: "Date is required");
+
+            invalidDebtException.AddData(
+                key: nameof(Debt.UpdatedDate),
+                values: "Date is required");
+
+            invalidDebtException.AddData(
+                key: nameof(Debt.CustomerId),
+                values: "Id is required");
+
+            invalidDebtException.AddData(
+                key: nameof(Debt.UserId),
+                values: "Id is required");
+
+            var expectedDebtValidationException =
+                new DebtValidationException(invalidDebtException);
+
+            // when
+            ValueTask<Debt> addDebtTask =
+                this.debtService.AddDebtAsync(invalidDebt);
+
+            DebtValidationException actualDebtValidationException =
+                await Assert.ThrowsAsync<DebtValidationException>(
+                    addDebtTask.AsTask);
+
+            // then
+            actualDebtValidationException.Should()
+                .BeEquivalentTo(expectedDebtValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDebtValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertDebtAsync(It.IsAny<Debt>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
