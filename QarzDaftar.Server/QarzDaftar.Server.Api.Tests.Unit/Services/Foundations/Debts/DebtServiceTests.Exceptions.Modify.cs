f23aae6d7b50fbd605f8371341b6eca228abf2ff
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-03 23:13:09 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Debts/DebtServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.Debts;
+using QarzDaftar.Server.Api.Models.Foundations.Debts.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Debts
+{
+    public partial class DebtServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Debt randomDebt = CreateRandomDebt();
+            Debt someDebt = randomDebt;
+            Guid DebtId = someDebt.Id;
+            SqlException sqlException = GetSqlError();
+
+            var failedDebtStorageException =
+                new FailedDebtStorageException(sqlException);
+
+            var expectedDebtDependencyException =
+                new DebtDependencyException(failedDebtStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectDebtByIdAsync(DebtId)).Throws(sqlException);
+
+            // when
+            ValueTask<Debt> modifyDebtTask =
+                this.debtService.ModifyDebtAsync(someDebt);
+
+            DebtDependencyException actualDebtDependencyException =
+                 await Assert.ThrowsAsync<DebtDependencyException>(
+                    modifyDebtTask.AsTask);
+
+            // then
+            actualDebtDependencyException.Should()
+                .BeEquivalentTo(expectedDebtDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedDebtDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectDebtByIdAsync(DebtId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateDebtAsync(someDebt), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
