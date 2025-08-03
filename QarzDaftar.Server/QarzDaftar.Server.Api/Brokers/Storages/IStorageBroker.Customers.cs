f1c21a85ef76fbce0bd0cbca66732418a90515e3
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-03 11:49:35 +0500

    BROKERS: Update Customer

--- QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/IStorageBroker.Customers.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/IStorageBroker.Customers.cs
@@ -7,5 +7,6 @@ namespace QarzDaftar.Server.Api.Brokers.Storages
         ValueTask<Customer> InsertCustomerAsync(Customer customer);
         IQueryable<Customer> SelectAllCustomers();
         ValueTask<Customer> SelectCustomerByIdAsync(Guid customerId);
+        ValueTask<Customer> UpdateCustomerAsync(Customer customer);
     }
 }
