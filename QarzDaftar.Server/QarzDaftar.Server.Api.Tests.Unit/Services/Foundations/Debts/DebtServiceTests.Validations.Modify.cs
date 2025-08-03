a5e34dc1cf8115729046f095fa9e6feb886d4152
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-03 22:50:39 +0500

    ShouldThrowValidationExceptionIfDebtIsNullAndLogItAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Debts/DebtServiceTests.Validations.Modify.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.Debts;
+using QarzDaftar.Server.Api.Models.Foundations.Debts.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Debts
+{
+    public partial class DebtServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionIfDebtIsNullAndLogItAsync()
+        {
+            // given
+            Debt nullDebt = null;
+            var nullDebtException = new NullDebtException();
+
+            var expectedDebtValidationException =
+                new DebtValidationException(nullDebtException);
+
+            // when
+            ValueTask<Debt> modifyDebtTask =
+                this.debtService.ModifyDebtAsync(nullDebt);
+
+            DebtValidationException actualDebtValidationException =
+                await Assert.ThrowsAsync<DebtValidationException>(() =>
+                    modifyDebtTask.AsTask());
+
+            // then
+            actualDebtValidationException.Should()
+                .BeEquivalentTo(expectedDebtValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedDebtValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateDebtAsync(It.IsAny<Debt>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
