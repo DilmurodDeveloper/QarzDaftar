1dfe6a9c83d58f61f2c7cbe975d89052ffbb742a
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-03 14:37:09 +0500

    ShouldThrowNotFoundExceptionOnRemoveCustomerByIdIsNotFoundAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Customers/CustomerServiceTests.Validations.RemoveById.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Customers/CustomerServiceTests.Validations.RemoveById.cs
@@ -46,5 +46,50 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Customers
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemoveCustomerByIdIsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid inputCustomerId = Guid.NewGuid();
+            Customer noCustomer = null;
+            var notFoundCustomerException = new NotFoundCustomerException(inputCustomerId);
+
+            var expectedCustomerValidationException =
+                new CustomerValidationException(notFoundCustomerException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCustomerByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noCustomer);
+
+            // when
+            ValueTask<Customer> removeCustomerById =
+                this.customerService.RemoveCustomerByIdAsync(inputCustomerId);
+
+            var actualCustomerValidationException =
+                await Assert.ThrowsAsync<CustomerValidationException>(
+                    removeCustomerById.AsTask);
+
+            // then
+            actualCustomerValidationException.Should()
+                .BeEquivalentTo(expectedCustomerValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCustomerByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedCustomerValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteCustomerAsync(It.IsAny<Customer>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
