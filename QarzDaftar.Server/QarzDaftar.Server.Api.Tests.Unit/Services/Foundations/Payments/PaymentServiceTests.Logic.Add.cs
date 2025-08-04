1b45c13da5f5c92d29d41222870a65d70adbc0ea
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 11:09:33 +0500

    ShouldAddPaymentAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Payments/PaymentServiceTests.Logic.Add.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.Payments;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Payments
+{
+    public partial class PaymentServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddPaymentAsync()
+        {
+            // given
+            Payment randomPayment = CreateRandomPayment();
+            Payment inputPayment = randomPayment;
+            Payment persistedPayment = inputPayment;
+            Payment expectedPayment = persistedPayment.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertPaymentAsync(inputPayment))
+                    .ReturnsAsync(persistedPayment);
+
+            // when
+            Payment actualPayment =
+                await this.paymentService.AddPaymentAsync(inputPayment);
+
+            // then
+            actualPayment.Should().BeEquivalentTo(expectedPayment);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertPaymentAsync(inputPayment), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Payments/PaymentServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using QarzDaftar.Server.Api.Brokers.DateTimes;
+using QarzDaftar.Server.Api.Brokers.Loggings;
+using QarzDaftar.Server.Api.Brokers.Storages;
+using QarzDaftar.Server.Api.Models.Foundations.Payments;
+using QarzDaftar.Server.Api.Services.Foundations.Payments;
+using Tynamix.ObjectFiller;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Payments
+{
+    public partial class PaymentServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly IPaymentService paymentService;
+
+        public PaymentServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.paymentService = new PaymentService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object);
+        }
+
+        private static Payment CreateRandomPayment() =>
+            CreatePaymentFiller(date: GetRandomDateTimeOffset()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Filler<Payment> CreatePaymentFiller(DateTimeOffset date)
+        {
+            var filler = new Filler<Payment>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(date);
+
+            return filler;
+        }
+    }
+}
