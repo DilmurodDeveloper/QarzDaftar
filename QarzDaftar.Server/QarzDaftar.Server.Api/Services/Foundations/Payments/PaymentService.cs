7752686ebb1f367b0929f5af0c4c3778aef990af
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 12:15:15 +0500

    CODE RUB: Implement TryCatch

--- QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundations/Payments/PaymentService.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundations/Payments/PaymentService.cs
@@ -1,10 +1,7 @@
-﻿using EFxceptions.Models.Exceptions;
-using Microsoft.Data.SqlClient;
-using QarzDaftar.Server.Api.Brokers.DateTimes;
+﻿using QarzDaftar.Server.Api.Brokers.DateTimes;
 using QarzDaftar.Server.Api.Brokers.Loggings;
 using QarzDaftar.Server.Api.Brokers.Storages;
 using QarzDaftar.Server.Api.Models.Foundations.Payments;
-using QarzDaftar.Server.Api.Models.Foundations.Payments.Exceptions;
 
 namespace QarzDaftar.Server.Api.Services.Foundations.Payments
 {
@@ -24,68 +21,12 @@ namespace QarzDaftar.Server.Api.Services.Foundations.Payments
             this.dateTimeBroker = dateTimeBroker;
         }
 
-        public async ValueTask<Payment> AddPaymentAsync(Payment payment)
+        public ValueTask<Payment> AddPaymentAsync(Payment payment) =>
+        TryCatch(async () =>
         {
-            try
-            {
-                ValidatePaymentOnAdd(payment);
+            ValidatePaymentOnAdd(payment);
 
-                return await this.storageBroker.InsertPaymentAsync(payment);
-            }
-            catch (NullPaymentException nullPaymentException)
-            {
-                var paymentValidationException =
-                    new PaymentValidationException(nullPaymentException);
-
-                this.loggingBroker.LogError(paymentValidationException);
-
-                throw paymentValidationException;
-            }
-            catch (InvalidPaymentException invalidPaymentException)
-            {
-                var paymentValidationException =
-                    new PaymentValidationException(invalidPaymentException);
-
-                this.loggingBroker.LogError(paymentValidationException);
-
-                throw paymentValidationException;
-            }
-            catch (SqlException sqlException)
-            {
-                var failedPaymentStorageException =
-                    new FailedPaymentStorageException(sqlException);
-
-                var paymentDependencyException =
-                    new PaymentDependencyException(failedPaymentStorageException);
-
-                this.loggingBroker.LogCritical(paymentDependencyException);
-
-                throw paymentDependencyException;
-            }
-            catch (DuplicateKeyException duplicateKeyException)
-            {
-                var alreadyExistsPaymentException =
-                    new AlreadyExistsPaymentException(duplicateKeyException);
-
-                var paymentDependencyValidationException =
-                    new PaymentDependencyValidationException(alreadyExistsPaymentException);
-
-                this.loggingBroker.LogError(paymentDependencyValidationException);
-
-                throw paymentDependencyValidationException;
-            }
-            catch (Exception exception)
-            {
-                var failedPaymentServiceException =
-                    new FailedPaymentServiceException(exception);
-
-                var paymentServiceException =
-                    new PaymentServiceException(failedPaymentServiceException);
-
-                this.loggingBroker.LogError(paymentServiceException);
-
-                throw paymentServiceException;
-            }
-        }
+            return await this.storageBroker.InsertPaymentAsync(payment);
+        });
     }
 }
