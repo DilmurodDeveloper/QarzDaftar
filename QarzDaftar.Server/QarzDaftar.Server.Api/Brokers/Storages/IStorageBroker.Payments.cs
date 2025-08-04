2a417b18b1de7f2da3670353f9e0a2116ea46a4d
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 16:10:18 +0500

    BROKERS: Update Payment

--- QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/IStorageBroker.Payments.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/IStorageBroker.Payments.cs
@@ -7,5 +7,6 @@ namespace QarzDaftar.Server.Api.Brokers.Storages
         ValueTask<Payment> InsertPaymentAsync(Payment payment);
         IQueryable<Payment> SelectAllPayments();
         ValueTask<Payment> SelectPaymentByIdAsync(Guid paymentId);
+        ValueTask<Payment> UpdatePaymentAsync(Payment payment);
     }
 }
