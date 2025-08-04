2b0073fed385b6f8d333e8c46c4429b34ddb994e
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 11:20:39 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Payments/PaymentServiceTests.Validations.Add.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.Payments;
+using QarzDaftar.Server.Api.Models.Foundations.Payments.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Payments
+{
+    public partial class PaymentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync()
+        {
+            // given
+            Payment nullPayment = null;
+            var nullPaymentException = new NullPaymentException();
+
+            var expectedPaymentValidationException =
+                new PaymentValidationException(nullPaymentException);
+
+            // when
+            ValueTask<Payment> addPaymentTask =
+                this.paymentService.AddPaymentAsync(nullPayment);
+
+            PaymentValidationException actualPaymentException =
+                await Assert.ThrowsAsync<PaymentValidationException>(
+                    addPaymentTask.AsTask);
+
+            // then
+            actualPaymentException.Should()
+                .BeEquivalentTo(expectedPaymentValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPaymentValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertPaymentAsync(It.IsAny<Payment>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Models/Foundations/Payments/Exceptions/PaymentValidationException.cs
@@ -0,0 +1,12 @@
+﻿using Xeptions;
+
+namespace QarzDaftar.Server.Api.Models.Foundations.Payments.Exceptions
+{
+    public class PaymentValidationException : Xeption
+    {
+        public PaymentValidationException(Xeption innerException)
+            : base(message: "Payment validation error occurred, fix the errors and try again.",
+                  innerException)
+        { }
+    }
+}
