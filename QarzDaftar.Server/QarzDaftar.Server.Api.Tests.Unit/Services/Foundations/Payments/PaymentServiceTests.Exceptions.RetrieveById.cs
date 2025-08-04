5de1566b0f26ca7e07966420dbb8300d773971e3
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 15:48:02 +0500

    ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Payments/PaymentServiceTests.Exceptions.RetrieveById.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Payments/PaymentServiceTests.Exceptions.RetrieveById.cs
@@ -50,5 +50,48 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Payments
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedPaymentServiceException =
+                new FailedPaymentServiceException(serviceException);
+
+            var expectedPaymentServiceException =
+                new PaymentServiceException(failedPaymentServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPaymentByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<Payment> retrievePaymentByIdTask =
+                this.paymentService.RetrievePaymentByIdAsync(someId);
+
+            PaymentServiceException actualPaymentServiceException =
+                await Assert.ThrowsAsync<PaymentServiceException>(
+                    retrievePaymentByIdTask.AsTask);
+
+            // then
+            actualPaymentServiceException.Should()
+                .BeEquivalentTo(expectedPaymentServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPaymentByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPaymentServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
