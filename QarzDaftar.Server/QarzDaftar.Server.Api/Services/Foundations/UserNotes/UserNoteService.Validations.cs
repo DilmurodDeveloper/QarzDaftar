098965ce53e1b66b4a98e78d917acb75b1f73554
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 22:19:19 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> PASS

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundations/UserNotes/UserNoteService.Validations.cs
@@ -0,0 +1,16 @@
+﻿using QarzDaftar.Server.Api.Models.Foundations.UserNotes;
+using QarzDaftar.Server.Api.Models.Foundations.UserNotes.Exceptions;
+
+namespace QarzDaftar.Server.Api.Services.Foundations.UserNotes
+{
+    public partial class UserNoteService
+    {
+        private static void ValidateUserNoteNotNull(UserNote UserNote)
+        {
+            if (UserNote is null)
+            {
+                throw new NullUserNoteException();
+            }
+        }
+    }
+}
--- QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundations/UserNotes/UserNoteService.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundations/UserNotes/UserNoteService.cs
@@ -2,10 +2,11 @@
 using QarzDaftar.Server.Api.Brokers.Loggings;
 using QarzDaftar.Server.Api.Brokers.Storages;
 using QarzDaftar.Server.Api.Models.Foundations.UserNotes;
+using QarzDaftar.Server.Api.Models.Foundations.UserNotes.Exceptions;
 
 namespace QarzDaftar.Server.Api.Services.Foundations.UserNotes
 {
-    public class UserNoteService : IUserNoteService
+    public partial class UserNoteService : IUserNoteService
     {
         private readonly IStorageBroker storageBroker;
         private readonly ILoggingBroker loggingBroker;
@@ -21,7 +22,23 @@ namespace QarzDaftar.Server.Api.Services.Foundations.UserNotes
             this.dateTimeBroker = dateTimeBroker;
         }
 
-        public async ValueTask<UserNote> AddUserNoteAsync(UserNote userNote) =>
-            await this.storageBroker.InsertUserNoteAsync(userNote);
+        public async ValueTask<UserNote> AddUserNoteAsync(UserNote userNote)
+        {
+            try
+            {
+                ValidateUserNoteNotNull(userNote);
+
+                return await this.storageBroker.InsertUserNoteAsync(userNote);
+            }
+            catch (NullUserNoteException nullUserNoteException)
+            {
+                var userNoteValidationException =
+                    new UserNoteValidationException(nullUserNoteException);
+
+                this.loggingBroker.LogError(userNoteValidationException);
+
+                throw userNoteValidationException;
+            }
+        }
     }
 }
