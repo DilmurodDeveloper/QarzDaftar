ee3b105f31ce977511921e3a366022c1c0df4397
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 15:04:40 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Payments/PaymentServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.Payments.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Payments
+{
+    public partial class PaymentServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given 
+            SqlException sqlException = GetSqlError();
+
+            var failedPaymentStorageException =
+                new FailedPaymentStorageException(sqlException);
+
+            var expectedPaymentDependencyException =
+                new PaymentDependencyException(failedPaymentStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllPayments()).Throws(sqlException);
+
+            // when
+            Action retrieveAllPaymentsAction = () =>
+                this.paymentService.RetrieveAllPayments();
+
+            PaymentDependencyException actualPaymentDependencyException =
+                Assert.Throws<PaymentDependencyException>(retrieveAllPaymentsAction);
+
+            // then
+            actualPaymentDependencyException.Should()
+                .BeEquivalentTo(expectedPaymentDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllPayments(), Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogCritical(It.Is(SameExceptionAs(
+                expectedPaymentDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
