39873573d0a5dea6e3020261bac9e3644a7fe2fa
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 12:25:20 +0500

    EXPOSERS: Post Payment

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Controllers/PaymentsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QarzDaftar.Server.Api.Models.Foundations.Payments;
+using QarzDaftar.Server.Api.Models.Foundations.Payments.Exceptions;
+using QarzDaftar.Server.Api.Services.Foundations.Payments;
+using RESTFulSense.Controllers;
+
+namespace QarzDaftar.Server.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentsController : RESTFulController
+    {
+        private readonly IPaymentService paymentService;
+
+        public PaymentsController(IPaymentService paymentService) =>
+            this.paymentService = paymentService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Payment>> PostPaymentAsync(Payment payment)
+        {
+            try
+            {
+                Payment addPayment = await this.paymentService.AddPaymentAsync(payment);
+
+                return Created(addPayment);
+            }
+            catch (PaymentValidationException paymentValidationException)
+            {
+                return BadRequest(paymentValidationException.InnerException);
+            }
+            catch (PaymentDependencyValidationException paymentDependencyValidationException)
+                when (paymentDependencyValidationException.InnerException is AlreadyExistsPaymentException)
+            {
+                return Conflict(paymentDependencyValidationException.InnerException);
+            }
+            catch (PaymentDependencyException paymentDependencyException)
+            {
+                return InternalServerError(paymentDependencyException.InnerException);
+            }
+            catch (PaymentServiceException paymentServiceException)
+            {
+                return InternalServerError(paymentServiceException.InnerException);
+            }
+        }
+    }
+}
