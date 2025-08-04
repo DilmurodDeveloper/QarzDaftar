af385bf5111ff901fa150ab1744dbb4e4f9d339f
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-04 18:44:01 +0500

    ShouldThrowNotFoundExceptionOnRemovePaymentByIdIsNotFoundAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Payments/PaymentServiceTests.Validations.RemoveById.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/Payments/PaymentServiceTests.Validations.RemoveById.cs
@@ -46,5 +46,50 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.Payments
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemovePaymentByIdIsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid inputPaymentId = Guid.NewGuid();
+            Payment noPayment = null;
+            var notFoundPaymentException = new NotFoundPaymentException(inputPaymentId);
+
+            var expectedPaymentValidationException =
+                new PaymentValidationException(notFoundPaymentException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPaymentByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noPayment);
+
+            // when
+            ValueTask<Payment> removePaymentById =
+                this.paymentService.RemovePaymentByIdAsync(inputPaymentId);
+
+            var actualPaymentValidationException =
+                await Assert.ThrowsAsync<PaymentValidationException>(
+                    removePaymentById.AsTask);
+
+            // then
+            actualPaymentValidationException.Should()
+                .BeEquivalentTo(expectedPaymentValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPaymentByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPaymentValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeletePaymentAsync(It.IsAny<Payment>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
