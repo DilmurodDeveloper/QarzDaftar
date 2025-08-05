9559c1a6ffbeb2e9c337c7dcf390f32983208a81
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 23:33:08 +0500

    BROKERS: Insert UserPaymentLog

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/IStorageBroker.UserPaymentLogs.cs
@@ -0,0 +1,9 @@
+﻿using QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs;
+
+namespace QarzDaftar.Server.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<UserPaymentLog> InsertUserPaymentLogAsync(UserPaymentLog userPaymentLog);
+    }
+}
--- QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/StorageBroker.UserPaymentLogs.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/StorageBroker.UserPaymentLogs.cs
@@ -6,5 +6,8 @@ namespace QarzDaftar.Server.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<UserPaymentLog> UserPaymentLogs { get; set; }
+
+        public async ValueTask<UserPaymentLog> InsertUserPaymentLogAsync(
+            UserPaymentLog userPaymentLog) => await InsertAsync(userPaymentLog);
     }
 }
