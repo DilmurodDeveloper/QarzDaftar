8a2f1e72742ced682509ac4e247481ad8ba47d4c
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 22:46:44 +0500

    ShouldThrowNotFoundExceptionOnRemoveSubscriptionHistoryByIdIsNotFoundAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SubscriptionHistories/SubscriptionHistoryServiceTests.Validations.RemoveById.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SubscriptionHistories/SubscriptionHistoryServiceTests.Validations.RemoveById.cs
@@ -46,5 +46,53 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.SubscriptionHist
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemoveSubscriptionHistoryByIdIsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid inputSubscriptionHistoryId = Guid.NewGuid();
+            SubscriptionHistory noSubscriptionHistory = null;
+            
+            var notFoundSubscriptionHistoryException =
+                new NotFoundSubscriptionHistoryException(inputSubscriptionHistoryId);
+
+            var expectedSubscriptionHistoryValidationException =
+                new SubscriptionHistoryValidationException(notFoundSubscriptionHistoryException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSubscriptionHistoryByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noSubscriptionHistory);
+
+            // when
+            ValueTask<SubscriptionHistory> removeSubscriptionHistoryById =
+                this.subscriptionHistoryService.RemoveSubscriptionHistoryByIdAsync(
+                    inputSubscriptionHistoryId);
+
+            var actualSubscriptionHistoryValidationException =
+                await Assert.ThrowsAsync<SubscriptionHistoryValidationException>(
+                    removeSubscriptionHistoryById.AsTask);
+
+            // then
+            actualSubscriptionHistoryValidationException.Should()
+                .BeEquivalentTo(expectedSubscriptionHistoryValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSubscriptionHistoryByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSubscriptionHistoryValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteSubscriptionHistoryAsync(It.IsAny<SubscriptionHistory>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
