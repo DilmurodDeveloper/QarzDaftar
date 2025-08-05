f3f2ded4facc7baae1c24c4a71771e83187f848f
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 12:05:21 +0500

    ShouldThrowValidationExceptionOnModifyIfUserNoteIsInvalidAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserNotes/UserNoteServiceTests.Validations.Modify.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserNotes/UserNoteServiceTests.Validations.Modify.cs
@@ -42,5 +42,67 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.UserNotes
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfUserNoteIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            //given
+            UserNote invalidUserNote = new UserNote
+            {
+                Content = invalidString
+            };
+
+            var invalidUserNoteException = new InvalidUserNoteException();
+
+            invalidUserNoteException.AddData(
+                key: nameof(UserNote.Id),
+                values: "Id is required");
+
+            invalidUserNoteException.AddData(
+                key: nameof(UserNote.Content),
+                values: "Text is required");
+
+            invalidUserNoteException.AddData(
+                key: nameof(UserNote.ReminderDate),
+                values: "Date is required");
+
+            invalidUserNoteException.AddData(
+                key: nameof(UserNote.CreatedAt),
+                values: "Date is required");
+
+            invalidUserNoteException.AddData(
+                key: nameof(UserNote.UserId),
+                values: "Id is required");
+
+            var expectedUserNoteValidationException =
+                new UserNoteValidationException(invalidUserNoteException);
+
+            // when
+            ValueTask<UserNote> modifyUserNoteTask =
+                this.userNoteService.ModifyUserNoteAsync(invalidUserNote);
+
+            UserNoteValidationException actualUserNoteValidationException =
+                await Assert.ThrowsAsync<UserNoteValidationException>(
+                    modifyUserNoteTask.AsTask);
+
+            // then
+            actualUserNoteValidationException.Should()
+                .BeEquivalentTo(expectedUserNoteValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserNoteValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateUserNoteAsync(It.IsAny<UserNote>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
