57cfc33595245ca572cc783e483f34280fe8d4ef
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 14:28:49 +0500

    ShouldThrowNotFoundExceptionOnRemoveUserNoteByIdIsNotFoundAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserNotes/UserNoteServiceTests.Validations.RemoveById.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserNotes/UserNoteServiceTests.Validations.RemoveById.cs
@@ -46,5 +46,50 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.UserNotes
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemoveUserNoteByIdIsNotFoundAndLogItAsync()
+        {
+            // given
+            Guid inputUserNoteId = Guid.NewGuid();
+            UserNote noUserNote = null;
+            var notFoundUserNoteException = new NotFoundUserNoteException(inputUserNoteId);
+
+            var expectedUserNoteValidationException =
+                new UserNoteValidationException(notFoundUserNoteException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserNoteByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noUserNote);
+
+            // when
+            ValueTask<UserNote> removeUserNoteById =
+                this.userNoteService.RemoveUserNoteByIdAsync(inputUserNoteId);
+
+            var actualUserNoteValidationException =
+                await Assert.ThrowsAsync<UserNoteValidationException>(
+                    removeUserNoteById.AsTask);
+
+            // then
+            actualUserNoteValidationException.Should()
+                .BeEquivalentTo(expectedUserNoteValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserNoteByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserNoteValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteUserNoteAsync(It.IsAny<UserNote>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
