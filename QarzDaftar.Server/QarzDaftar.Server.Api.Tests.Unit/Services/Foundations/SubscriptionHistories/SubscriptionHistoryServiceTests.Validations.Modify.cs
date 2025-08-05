1a1d2a88ff25884aa5c48953c09e2183428b4f16
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 20:09:23 +0500

    ShouldThrowValidationExceptionIfSubscriptionHistoryIsNullAndLogItAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SubscriptionHistories/SubscriptionHistoryServiceTests.Validations.Modify.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.SubscriptionHistories;
+using QarzDaftar.Server.Api.Models.Foundations.SubscriptionHistories.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.SubscriptionHistories
+{
+    public partial class SubscriptionHistoryServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionIfSubscriptionHistoryIsNullAndLogItAsync()
+        {
+            // given
+            SubscriptionHistory nullSubscriptionHistory = null;
+            var nullSubscriptionHistoryException = new NullSubscriptionHistoryException();
+
+            var expectedSubscriptionHistoryValidationException =
+                new SubscriptionHistoryValidationException(nullSubscriptionHistoryException);
+
+            // when
+            ValueTask<SubscriptionHistory> modifySubscriptionHistoryTask =
+                this.subscriptionHistoryService.ModifySubscriptionHistoryAsync(nullSubscriptionHistory);
+
+            SubscriptionHistoryValidationException actualSubscriptionHistoryValidationException =
+                await Assert.ThrowsAsync<SubscriptionHistoryValidationException>(
+                    modifySubscriptionHistoryTask.AsTask);
+
+            // then
+            actualSubscriptionHistoryValidationException.Should()
+                .BeEquivalentTo(expectedSubscriptionHistoryValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSubscriptionHistoryValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateSubscriptionHistoryAsync(It.IsAny<SubscriptionHistory>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
