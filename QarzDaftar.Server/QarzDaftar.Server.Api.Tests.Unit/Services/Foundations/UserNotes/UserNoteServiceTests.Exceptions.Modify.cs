fb31c48312fe117dcc878f96cda0fe2f7c8b95ef
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 12:14:12 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserNotes/UserNoteServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.UserNotes;
+using QarzDaftar.Server.Api.Models.Foundations.UserNotes.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.UserNotes
+{
+    public partial class UserNoteServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            UserNote randomUserNote = CreateRandomUserNote();
+            UserNote someUserNote = randomUserNote;
+            Guid UserNoteId = someUserNote.Id;
+            SqlException sqlException = GetSqlError();
+
+            var failedUserNoteStorageException =
+                new FailedUserNoteStorageException(sqlException);
+
+            var expectedUserNoteDependencyException =
+                new UserNoteDependencyException(failedUserNoteStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserNoteByIdAsync(UserNoteId)).Throws(sqlException);
+
+            // when
+            ValueTask<UserNote> modifyUserNoteTask =
+                this.userNoteService.ModifyUserNoteAsync(someUserNote);
+
+            UserNoteDependencyException actualUserNoteDependencyException =
+                 await Assert.ThrowsAsync<UserNoteDependencyException>(
+                    modifyUserNoteTask.AsTask);
+
+            // then
+            actualUserNoteDependencyException.Should()
+                .BeEquivalentTo(expectedUserNoteDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedUserNoteDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserNoteByIdAsync(UserNoteId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateUserNoteAsync(someUserNote), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
