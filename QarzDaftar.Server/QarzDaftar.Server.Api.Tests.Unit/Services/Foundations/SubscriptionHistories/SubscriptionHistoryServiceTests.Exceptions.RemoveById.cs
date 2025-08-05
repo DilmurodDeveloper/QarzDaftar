af39b3401b8b622d252f07df6773e11907168083
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 22:55:17 +0500

    ShouldThrowServiceExceptionOnRemoveIfExceptionOccursAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SubscriptionHistories/SubscriptionHistoryServiceTests.Exceptions.RemoveById.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SubscriptionHistories/SubscriptionHistoryServiceTests.Exceptions.RemoveById.cs
@@ -94,5 +94,48 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.SubscriptionHist
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRemoveIfExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someSubscriptionHistoryId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedSubscriptionHistoryServiceException =
+                new FailedSubscriptionHistoryServiceException(serviceException);
+
+            var expectedSubscriptionHistoryServiceException =
+                new SubscriptionHistoryServiceException(failedSubscriptionHistoryServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSubscriptionHistoryByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<SubscriptionHistory> removeSubscriptionHistoryByIdTask =
+                this.subscriptionHistoryService.RemoveSubscriptionHistoryByIdAsync(someSubscriptionHistoryId);
+
+            SubscriptionHistoryServiceException actualSubscriptionHistoryServiceException =
+                await Assert.ThrowsAsync<SubscriptionHistoryServiceException>(
+                    removeSubscriptionHistoryByIdTask.AsTask);
+
+            // then
+            actualSubscriptionHistoryServiceException.Should()
+                .BeEquivalentTo(expectedSubscriptionHistoryServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSubscriptionHistoryByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSubscriptionHistoryServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
