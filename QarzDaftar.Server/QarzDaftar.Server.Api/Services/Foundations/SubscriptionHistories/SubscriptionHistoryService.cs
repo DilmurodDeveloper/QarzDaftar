4b6f435627d5b42dd7c6dddc180fccd15328fda2
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 19:13:59 +0500

    ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync -> PASS

--- QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundations/SubscriptionHistories/SubscriptionHistoryService.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Services/Foundations/SubscriptionHistories/SubscriptionHistoryService.cs
@@ -80,6 +80,18 @@ namespace QarzDaftar.Server.Api.Services.Foundations.SubscriptionHistories
 
                 throw subscriptionHistoryDependencyException;
             }
+            catch (Exception exception)
+            {
+                var failedSubscriptionHistoryServiceException =
+                    new FailedSubscriptionHistoryServiceException(exception);
+
+                var subscriptionHistoryServiceException =
+                    new SubscriptionHistoryServiceException(failedSubscriptionHistoryServiceException);
+
+                this.loggingBroker.LogError(subscriptionHistoryServiceException);
+
+                throw subscriptionHistoryServiceException;
+            }
         }
     }
 }
