5e84822484594ca0197e2152a3bb8cef1f75545a
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 16:24:07 +0500

    ShouldThrowValidationExceptionOnAddIfSubscriptionHistoryIsInvalidAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SubscriptionHistories/SubscriptionHistoryServiceTests.Validations.Add.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SubscriptionHistories/SubscriptionHistoryServiceTests.Validations.Add.cs
@@ -44,5 +44,67 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.SubscriptionHist
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public async Task ShouldThrowValidationExceptionOnAddIfSubscriptionHistoryIsInvalidAndLogItAsync(
+            decimal invalidAmount)
+        {
+            // given
+            var invalidSubscriptionHistory = new SubscriptionHistory
+            {
+                Amount = invalidAmount,
+            };
+
+            var invalidSubscriptionHistoryException = new InvalidSubscriptionHistoryException();
+
+            invalidSubscriptionHistoryException.AddData(
+                key: nameof(SubscriptionHistory.Id),
+                values: "Id is required");
+
+            invalidSubscriptionHistoryException.AddData(
+                key: nameof(SubscriptionHistory.Amount),
+                values: "Amount is invalid");
+
+            invalidSubscriptionHistoryException.AddData(
+                key: nameof(SubscriptionHistory.PurchasedAt),
+                values: "Date is required");
+
+            invalidSubscriptionHistoryException.AddData(
+                key: nameof(SubscriptionHistory.ExpiresAt),
+                values: "Date is required");
+
+            invalidSubscriptionHistoryException.AddData(
+                key: nameof(SubscriptionHistory.UserId),
+                values: "Id is required");
+
+            var expectedSubscriptionHistoryValidationException =
+                new SubscriptionHistoryValidationException(invalidSubscriptionHistoryException);
+
+            // when
+            ValueTask<SubscriptionHistory> addSubscriptionHistoryTask =
+                this.subscriptionHistoryService.AddSubscriptionHistoryAsync(invalidSubscriptionHistory);
+
+            SubscriptionHistoryValidationException actualSubscriptionHistoryValidationException =
+                await Assert.ThrowsAsync<SubscriptionHistoryValidationException>(
+                    addSubscriptionHistoryTask.AsTask);
+
+            // then
+            actualSubscriptionHistoryValidationException.Should()
+                .BeEquivalentTo(expectedSubscriptionHistoryValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSubscriptionHistoryValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertSubscriptionHistoryAsync(It.IsAny<SubscriptionHistory>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
