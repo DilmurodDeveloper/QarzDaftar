75684ff28530978cef8423aecc9d0d93667f1582
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-05 23:53:57 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserPaymentLogs/UserPaymentLogServiceTests.Validations.Add.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs;
+using QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.UserPaymentLogs
+{
+    public partial class UserPaymentLogServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync()
+        {
+            // given
+            UserPaymentLog nullUserPaymentLog = null;
+            var nullUserPaymentLogException = new NullUserPaymentLogException();
+
+            var expectedUserPaymentLogValidationException =
+                new UserPaymentLogValidationException(nullUserPaymentLogException);
+
+            // when
+            ValueTask<UserPaymentLog> addUserPaymentLogTask =
+                this.userPaymentLogService.AddUserPaymentLogAsync(nullUserPaymentLog);
+
+            UserPaymentLogValidationException actualUserPaymentLogException =
+                await Assert.ThrowsAsync<UserPaymentLogValidationException>(
+                    addUserPaymentLogTask.AsTask);
+
+            // then
+            actualUserPaymentLogException.Should()
+                .BeEquivalentTo(expectedUserPaymentLogValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserPaymentLogValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserPaymentLogAsync(It.IsAny<UserPaymentLog>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Models/Foundations/UserPaymentLogs/Exceptions/NullUserPaymentLogException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs.Exceptions
+{
+    public class NullUserPaymentLogException : Xeption
+    {
+        public NullUserPaymentLogException()
+            : base(message: "User payment log is null.")
+        { }
+    }
+}
