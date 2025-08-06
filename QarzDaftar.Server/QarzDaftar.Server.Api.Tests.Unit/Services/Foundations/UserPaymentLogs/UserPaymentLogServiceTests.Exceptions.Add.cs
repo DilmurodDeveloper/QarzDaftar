67b3da281e8678956320a40f7fc256d0127c7c9b
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-06 10:51:52 +0500

    ShouldThrowDependencyValidationExceptionOnAddIfDuplicateKeyErrorOccursAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserPaymentLogs/UserPaymentLogServiceTests.Exceptions.Add.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserPaymentLogs/UserPaymentLogServiceTests.Exceptions.Add.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using EFxceptions.Models.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
 using Moq;
 using QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs;
 using QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs.Exceptions;
@@ -38,6 +40,47 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.UserPaymentLogs
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
                     expectedUserPaymentLogDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnAddIfDuplicateKeyErrorOccursAndLogItAsync()
+        {
+            // given
+            string someMessage = GetRandomString();
+            UserPaymentLog someUserPaymentLog = CreateRandomUserPaymentLog();
+            var duplicateKeyException = new DuplicateKeyException(someMessage);
+
+            var alreadyExistsUserPaymentLogException =
+                new AlreadyExistsUserPaymentLogException(duplicateKeyException);
+
+            var expectedUserPaymentLogDependencyValidationException =
+                new UserPaymentLogDependencyValidationException(alreadyExistsUserPaymentLogException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertUserPaymentLogAsync(someUserPaymentLog)).ThrowsAsync(duplicateKeyException);
+            // when
+            ValueTask<UserPaymentLog> addUserPaymentLogTask =
+                this.userPaymentLogService.AddUserPaymentLogAsync(someUserPaymentLog);
+
+            UserPaymentLogDependencyValidationException actualUserPaymentLogDependencyValidationException =
+                await Assert.ThrowsAsync<UserPaymentLogDependencyValidationException>(
+                    addUserPaymentLogTask.AsTask);
+
+            // then
+            actualUserPaymentLogDependencyValidationException.Should()
+                .BeEquivalentTo(expectedUserPaymentLogDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserPaymentLogAsync(someUserPaymentLog), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserPaymentLogDependencyValidationException))),
+                        Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Models/Foundations/UserPaymentLogs/Exceptions/UserPaymentLogDependencyValidationException.cs
@@ -0,0 +1,12 @@
+﻿using Xeptions;
+
+namespace QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs.Exceptions
+{
+    public class UserPaymentLogDependencyValidationException : Xeption
+    {
+        public UserPaymentLogDependencyValidationException(Xeption innerException)
+            : base(message: "User payment log dependency validation error occurred, fix the errors and try again.",
+                 innerException)
+        { }
+    }
+}
