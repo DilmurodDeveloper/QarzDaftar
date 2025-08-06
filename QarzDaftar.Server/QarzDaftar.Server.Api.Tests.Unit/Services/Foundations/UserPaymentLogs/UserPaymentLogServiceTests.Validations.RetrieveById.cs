3dc428d72ab6b0b2156926ab7184f0c3d2a97b9e
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-06 12:27:02 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserPaymentLogs/UserPaymentLogServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs;
+using QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs.Exceptions;
+
+namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.UserPaymentLogs
+{
+    public partial class UserPaymentLogServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidUserPaymentLogId = Guid.Empty;
+            var invalidUserPaymentLogException = new InvalidUserPaymentLogException();
+
+            invalidUserPaymentLogException.AddData(
+                key: nameof(UserPaymentLog.Id),
+                values: "Id is required");
+
+            var expectedUserPaymentLogValidationException =
+                new UserPaymentLogValidationException(invalidUserPaymentLogException);
+
+            // when
+            ValueTask<UserPaymentLog> retrieveUserPaymentLogById =
+                this.userPaymentLogService.RetrieveUserPaymentLogByIdAsync(
+                    invalidUserPaymentLogId);
+
+            var actualUserPaymentLogValidationException =
+                await Assert.ThrowsAsync<UserPaymentLogValidationException>(
+                    retrieveUserPaymentLogById.AsTask);
+
+            // then
+            actualUserPaymentLogValidationException.Should()
+                .BeEquivalentTo(expectedUserPaymentLogValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserPaymentLogValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserPaymentLogByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
