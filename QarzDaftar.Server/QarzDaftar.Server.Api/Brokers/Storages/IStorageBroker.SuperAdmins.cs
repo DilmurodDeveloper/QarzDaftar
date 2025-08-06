7cd171e36c9716e61124c26cbca6d901052f27ed
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-06 17:32:40 +0500

    BROKERS: Select SuperAdmin

---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Brokers/Storages/IStorageBroker.SuperAdmins.cs
@@ -0,0 +1,9 @@
+﻿using QarzDaftar.Server.Api.Models.Foundations.SuperAdmins;
+
+namespace QarzDaftar.Server.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<SuperAdmin> SelectSuperAdminByUsernameAsync(string username);
+    }
+}
