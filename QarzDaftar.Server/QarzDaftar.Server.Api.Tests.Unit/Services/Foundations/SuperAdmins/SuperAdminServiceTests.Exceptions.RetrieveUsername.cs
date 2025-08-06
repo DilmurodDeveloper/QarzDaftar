196a96add0a0d0ee6e8fd458100392bb5529ab7b
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-06 18:42:29 +0500

    ShouldThrowServiceExceptionOnRetrieveByUsernameIfServiceErrorOccursAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SuperAdmins/SuperAdminServiceTests.Exceptions.RetrieveUsername.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SuperAdmins/SuperAdminServiceTests.Exceptions.RetrieveUsername.cs
@@ -44,5 +44,42 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.SuperAdmins
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByUsernameIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            string someUsername = GetRandomUsername();
+            var serviceException = new Exception();
+
+            var failedServiceException = new FailedSuperAdminServiceException(serviceException);
+            var expectedServiceException = new SuperAdminServiceException(failedServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSuperAdminByUsernameAsync(It.IsAny<string>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<SuperAdmin> retrieveSuperAdminTask =
+                this.superAdminService.RetrieveSuperAdminByUsernameAsync(someUsername);
+
+            SuperAdminServiceException actualServiceException =
+                await Assert.ThrowsAsync<SuperAdminServiceException>(
+                    retrieveSuperAdminTask.AsTask);
+
+            // then
+            actualServiceException.Should().BeEquivalentTo(expectedServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSuperAdminByUsernameAsync(It.IsAny<string>()),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedServiceException))),
+                Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
