51a25904888e3f7b0489d8113a0c6608af4104e8
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-06 18:37:22 +0500

    ShouldThrowDependencyExceptionOnRetrieveByUsernameIfSqlErrorOccursAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SuperAdmins/SuperAdminServiceTests.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/SuperAdmins/SuperAdminServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Microsoft.Data.SqlClient;
 using Moq;
 using QarzDaftar.Server.Api.Brokers.Loggings;
 using QarzDaftar.Server.Api.Brokers.Storages;
@@ -35,6 +37,9 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.SuperAdmins
 
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
+
+        private static SqlException GetSqlError() =>
+            (SqlException)RuntimeHelpers.GetUninitializedObject(typeof(SqlException));
 
         private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
             actualException => actualException.SameExceptionAs(expectedException);
---
+++ QarzDaftar.Server/QarzDaftar.Server.Api/Models/Foundations/SuperAdmins/Exceptions/FailedSuperAdminStorageException.cs
@@ -0,0 +1,12 @@
+﻿using Xeptions;
+
+namespace QarzDaftar.Server.Api.Models.Foundations.SuperAdmins.Exceptions
+{
+    public class FailedSuperAdminStorageException : Xeption
+    {
+        public FailedSuperAdminStorageException(Exception innerException)
+            : base(message: "Failed super admin storage error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
