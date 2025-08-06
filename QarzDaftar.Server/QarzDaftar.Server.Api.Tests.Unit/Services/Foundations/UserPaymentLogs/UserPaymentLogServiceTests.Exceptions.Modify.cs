e564c31fa9c0f3c8f96c4494bd4cbdda3458012b
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-06 15:58:35 +0500

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserPaymentLogs/UserPaymentLogServiceTests.Exceptions.Modify.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserPaymentLogs/UserPaymentLogServiceTests.Exceptions.Modify.cs
@@ -1,6 +1,9 @@
 ﻿using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
+using QarzDaftar.Server.Api.Models.Foundations.SubscriptionHistories;
+using QarzDaftar.Server.Api.Models.Foundations.SubscriptionHistories.Exceptions;
 using QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs;
 using QarzDaftar.Server.Api.Models.Foundations.UserPaymentLogs.Exceptions;
 
@@ -51,6 +54,52 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.UserPaymentLogs
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateUserPaymentLogAsync(someUserPaymentLog),
                     Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            UserPaymentLog randomUserPaymentLog = CreateRandomUserPaymentLog();
+            UserPaymentLog someUserPaymentLog = randomUserPaymentLog;
+            Guid userPaymentLogId = someUserPaymentLog.Id;
+            var databaseUpdateException = new DbUpdateException();
+
+            var failedUserPaymentLogStorageException =
+                new FailedUserPaymentLogStorageException(databaseUpdateException);
+
+            var expectedUserPaymentLogDependencyException =
+                new UserPaymentLogDependencyException(failedUserPaymentLogStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserPaymentLogByIdAsync(userPaymentLogId))
+                    .Throws(databaseUpdateException);
+
+            // when
+            ValueTask<UserPaymentLog> modifyUserPaymentLogTask =
+                this.userPaymentLogService.ModifyUserPaymentLogAsync(someUserPaymentLog);
+
+            UserPaymentLogDependencyException actualUserPaymentLogDependencyException =
+                 await Assert.ThrowsAsync<UserPaymentLogDependencyException>(
+                    modifyUserPaymentLogTask.AsTask);
+
+            // then
+            actualUserPaymentLogDependencyException.Should()
+                .BeEquivalentTo(expectedUserPaymentLogDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserPaymentLogDependencyException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserPaymentLogByIdAsync(userPaymentLogId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateUserPaymentLogAsync(someUserPaymentLog), Times.Never);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
