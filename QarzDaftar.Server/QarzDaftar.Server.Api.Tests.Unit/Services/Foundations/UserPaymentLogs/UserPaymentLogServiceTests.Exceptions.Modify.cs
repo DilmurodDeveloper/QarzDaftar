e3e182330b32a0c7a89e30914912e0d3b8e7a969
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-08-06 16:02:59 +0500

    ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> FAIL

--- QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserPaymentLogs/UserPaymentLogServiceTests.Exceptions.Modify.cs
+++ QarzDaftar.Server/QarzDaftar.Server.Api.Tests.Unit/Services/Foundations/UserPaymentLogs/UserPaymentLogServiceTests.Exceptions.Modify.cs
@@ -103,5 +103,51 @@ namespace QarzDaftar.Server.Api.Tests.Unit.Services.Foundations.UserPaymentLogs
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            UserPaymentLog randomUserPaymentLog = CreateRandomUserPaymentLog();
+            UserPaymentLog someUserPaymentLog = randomUserPaymentLog;
+            Guid UserPaymentLogId = someUserPaymentLog.Id;
+            var dbUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedUserPaymentLogException =
+                new LockedUserPaymentLogException(dbUpdateConcurrencyException);
+
+            var expectedUserPaymentLogDependencyValidationException =
+                new UserPaymentLogDependencyValidationException(lockedUserPaymentLogException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserPaymentLogByIdAsync(UserPaymentLogId))
+                    .Throws(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<UserPaymentLog> modifyUserPaymentLogTask =
+                this.userPaymentLogService.ModifyUserPaymentLogAsync(someUserPaymentLog);
+
+            UserPaymentLogDependencyValidationException actualUserPaymentLogDependencyValidationException =
+                 await Assert.ThrowsAsync<UserPaymentLogDependencyValidationException>(
+                    modifyUserPaymentLogTask.AsTask);
+
+            // then
+            actualUserPaymentLogDependencyValidationException.Should()
+                .BeEquivalentTo(expectedUserPaymentLogDependencyValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserPaymentLogDependencyValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserPaymentLogByIdAsync(UserPaymentLogId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateUserPaymentLogAsync(someUserPaymentLog), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
